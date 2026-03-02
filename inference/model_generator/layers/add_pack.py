cdf5977f44a95dfb73e51432884214e53ae6a9d8
wangy <ying.wang@cipherflow.cn>
2026-03-02 17:03:01 +0800

    refactor: add_pack.py

--- inference/model_generator/layers/add_pack.py
+++ inference/model_generator/layers/add_pack.py
@@ -29,28 +29,6 @@ class AddLayer:
     def __init__(self):
         return
 
-    def mult_and_add(self, x1: DataNode, x2: DataNode, pt_scale1: DataNode, pt_scale2: DataNode = None):
-        mult_res1 = mult(x1, pt_scale1)
-        mult_res1 = rescale(mult_res1)
-        if pt_scale2 is not None:
-            mult_res2 = mult(x2, pt_scale2)
-            mult_res2 = rescale(mult_res2)
-            if mult_res2.level < mult_res1.level:
-                drop_level_n = mult_res1.level - mult_res2.level
-                mult_res1 = drop_level(mult_res1, drop_level_n)
-            elif mult_res1.level < mult_res2.level:
-                drop_level_n = mult_res2.level - mult_res1.level
-                mult_res2 = drop_level(mult_res2, drop_level_n)
-            return add(mult_res1, mult_res2)
-        else:
-            if x2.level < mult_res1.level:
-                drop_level_n = mult_res1.level - x2.level
-                mult_res1 = drop_level(mult_res1, drop_level_n)
-            elif mult_res1.level < x2.level:
-                drop_level_n = x2.level - mult_res1.level
-                x2 = drop_level(x2, drop_level_n)
-            return add(mult_res1, x2)
-
     def call(
         self,
         x1: list[DataNode],
@@ -74,13 +52,4 @@ class AddLayer:
                 elif x2[i].level == x1[i].level:
                     res = add(x1[i], x2[i])
                 result.append(res)
-        elif scale1 == 1.0 and scale2 != 1.0:
-            for i in range(len(x1)):
-                result.append(self.mult_and_add(x2[i], x1[i], pt_scale2))
-        elif scale1 != 1.0 and scale2 == 1.0:
-            for i in range(len(x1)):
-                result.append(self.mult_and_add(x1[i], x2[i], pt_scale1))
-        else:
-            for i in range(len(x1)):
-                result.append(self.mult_and_add(x1[i], x2[i], pt_scale1, pt_scale2))
         return result
