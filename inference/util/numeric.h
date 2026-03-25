a4f8f04a3926ab8464a0f42792e90b0bfc14df7c
Si Chen <si.chen@cipherflow.cn>
2026-03-25 13:44:49 +0800

    refactor: add numeric.h

---
+++ inference/util/numeric.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2025-2026 CipherFlow (Shenzhen) Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <cassert>
+#include <cstdint>
+#include <cmath>
+
+inline uint32_t div_ceil(uint32_t x, uint32_t q) {
+    assert(q != 0 && "div_ceil: division by zero");
+    return (x + q - 1) / q;
+}
+
+inline bool f_equal(double a, double b) {
+    const double eps = 1e-8;
+    if (fabs(b) < eps) {
+        return fabs(a - b) < eps;
+    } else {
+        return fabs((a - b) / b) < eps;
+    }
+}
